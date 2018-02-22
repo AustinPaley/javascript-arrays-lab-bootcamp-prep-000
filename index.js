14ac80c4369a2f7641b982be86283c0c86103e03
Austin Paley <austinpaley32@gmail.com>
2018-02-22 18:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.splice(2);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.splice(0);
+  return kittens;
+}
